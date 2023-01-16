abbe83a735ef1aaba02f91108b014b550f107da5
Rafael Alvarez <apiux@MacBook-Pro-de-APIUXNB1203.local>
2023-01-16 14:32:33 -0500

    Listo el metodo para eliminar empleados por id, devolviendole un status 204 cuando todo es exitoso y validando el error

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -18,6 +18,17 @@ export const getEmployeeById = async (req, res) => {
   res.json(rows[0]);
 };
 
+export const deleteEmployees = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.affectedRows <= 0) {
+    return res.status(404).json({ status: 404, message: "Employee not found" });
+  }
+  // 204 significa que todo salio bien pero no hay contenido
+  res.sendStatus(204);
+};
+
 //podemos insertar uno a uno los valores o podemos insertar un objeto
 export const createEmployees = async (req, res) => {
   const { name, salary } = req.body;
@@ -32,8 +43,4 @@ export const createEmployees = async (req, res) => {
 
 export const updateEmployees = (req, res) => {
   res.send("Actualizando empleados");
-};
-
-export const deleteEmployees = (req, res) => {
-  res.send("Eliminando empleados");
 };
